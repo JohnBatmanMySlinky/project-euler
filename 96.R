8f855ee03469e694a044725a2ed3074e518b362c
johnmyslinski <jmyslinski@munichre.com>
2020-08-24 03:12:29 +0000

    2nd while loop

--- 96.R
+++ 96.R
@@ -1,9 +1,11 @@
 rm(list = ls())
 
+library(tidyverse)
+
 `%notin%` = Negate(`%in%`)
 
 # download.file("https://projecteuler.net/project/resources/p096_sudoku.txt",
-#               "number_96.txt")
+#              "number_96.txt")
 
 # read in data into list of 50 matrices
 dat <- matrix(readLines("number_96.txt", warn = F), ncol = 10, byrow = T)
@@ -30,32 +32,45 @@ possible_numbers <- function(sdk, i, x, y){
   }
 }
 
-# check if given board, cell are OK, return bool
-ok <- function(sdk, i, x, y){
-  vert <- sdk[[i]][x,]
-  horz <- sdk[[i]][,y]
-  TbyT <- sdk[[i]][trunc((x-1e-4)/3)*3+1:3,trunc((y-1e-4)/3)*3+1:3]
-  
-  vert_test <- all(sort(vert) == 1:9)
-  horz_test <- all(sort(horz) == 1:9)
-  TbyT_test <- all(sort(TbyT) == 1:9)
-  
-  return(vert_test & horz_test & TbyT_test)
+#check if given board, cell are OK, return bool
+unique_solution <- function(sdk, i, x, y){
+  if (nchar(sdk[[i]][x,y])>1){
+    #horizontal
+    horz <- sdk[[i]][x,]
+    horz <- horz[nchar(horz) > 1]
+    horz <- unlist(strsplit(horz, ""))
+    horz <- horz[!(duplicated(horz) | duplicated(horz, fromLast = TRUE))]
+
+    #vertical
+    vert <- sdk[[i]][,y]
+    vert <- vert[nchar(vert) > 1]
+    vert <- unlist(strsplit(vert, ""))
+    vert <- vert[!(duplicated(vert) | duplicated(vert, fromLast = TRUE))]
+
+    #three by three
+    TbyT <- sdk[[i]][trunc((x-1e-4)/3)*3+1:3,
+                     trunc((y-1e-4)/3)*3+1:3]
+    TbyT <- TbyT[nchar(TbyT) > 1]
+    TbyT <- unlist(strsplit(TbyT, ""))
+    TbyT <- TbyT[!(duplicated(TbyT) | duplicated(TbyT, fromLast = TRUE))]
+
+    # final check
+    check <- unlist(strsplit(sdk[[i]][x,y],""))
+    
+    unq_sol <- paste0(check[check %in% c(TbyT, vert, horz)], collapse ="")
+    
+    if (nchar(unq_sol) == 0){
+      return(sdk[[i]][x,y])
+    } else {
+      return(unq_sol)
+    }
+  } else {
+    return(sdk[[i]][x,y])
+  }
 }
-# an actually correct sudoku board
-# sdk <- list(matrix(c(4,8,3,9,2,1,6,5,7,
-#                      9,6,7,3,4,5,8,2,1,
-#                      2,5,1,8,7,6,4,9,3,
-#                      5,4,8,1,3,2,9,7,6,
-#                      7,2,9,5,6,4,1,3,8,
-#                      1,3,6,7,9,8,2,4,5,
-#                      3,7,2,6,8,9,5,1,4,
-#                      8,1,4,2,5,3,7,6,9,
-#                      6,9,5,4,1,7,3,8,2), nrow = 9, ncol = 9))
-# i <- 1; x <- 1; y <- 1
 
 
-# fill sudoku_possible with possible numbers
+# initialize possible numbers
 sudoku_possible <- sudoku
 for (i in 1:50){
   for (x in 1:9){
@@ -66,10 +81,10 @@ for (i in 1:50){
 }
 
 
-
-# pick off easy answers
+# let the solve begin
 sudoku_output <- sudoku
 for (i in 1:50){
+  # begin smart pre-processing
   count_of_1s_post <- 1
   count_of_1s_pre <- 0
   while (count_of_1s_post > count_of_1s_pre){
@@ -83,9 +98,39 @@ for (i in 1:50){
         sudoku_possible[[i]][x,y] <- possible_numbers(sudoku_output, i, x, y)
       }
     } 
-    
     count_of_1s_post <- sum(nchar(sudoku_possible[[i]])==1)
   }
-  print(paste0("Pre-Brute Round: ", i," Completed Tiles:",count_of_1s_post))
+  
+  #after the while loop check for unique solutions
+  for (x in 1:9){
+    for (y in 1:9){
+      sudoku_possible[[i]][x,y] <- unique_solution(sudoku_possible, i, x, y)
+    }
+  }
+  
+  # begin smart pre-processing
+  count_of_1s_post <- 1
+  count_of_1s_pre <- 0
+  while (count_of_1s_post > count_of_1s_pre){
+    count_of_1s_pre <- sum(nchar(sudoku_possible[[i]])==1)
+    
+    index_dumb <- nchar(sudoku_possible[[i]]) == 1
+    sudoku_output[[i]][index_dumb] <- as.numeric(sudoku_possible[[i]][index_dumb])
+    
+    for (x in 1:9){
+      for (y in 1:9){
+        sudoku_possible[[i]][x,y] <- possible_numbers(sudoku_output, i, x, y)
+      }
+    } 
+    count_of_1s_post <- sum(nchar(sudoku_possible[[i]])==1)
+  }
+  
+  
+  print(paste0("Pre-Brute Round: ", i," Completed Tiles:",sum(nchar(sudoku_possible[[i]])==1)))
+  
+  
+  # begin brute force
 }
 
+
+sudoku_output[[1]]
