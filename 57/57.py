762c9a70235e24c6e6e7b83d08a5dd66f1e8b305
John Myslinski <johnmyslinski@Johns-Air.attlocal.net>
2020-10-11 18:55:23 -0500

    lol 6 lines

--- 57/57.py
+++ 57/57.py
@@ -1,7 +1,6 @@
-x = [1,2]; i = 0; winners = 0
-while i < 999:
+x = [1,2]; winners = 0
+for z in range(0,1000):
     x = [x[1],x[0]+2*x[1]]
-    i += 1
     if len(str(x[0]+x[1])) > len(str(x[1])):
         winners += 1
 print(winners)
