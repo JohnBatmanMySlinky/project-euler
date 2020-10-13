43c50f1830544e5ab059d82e32437703228fa294
John Myslinski <johnmyslinski@Johns-Air.attlocal.net>
2020-10-12 19:33:02 -0500

    haha 5 lines

--- 57/57.py
+++ 57/57.py
@@ -1,6 +1,5 @@
 x = [1,2]; winners = 0
 for z in range(0,1000):
     x = [x[1],x[0]+2*x[1]]
-    if len(str(x[0]+x[1])) > len(str(x[1])):
-        winners += 1
+    winners += 1*(len(str(x[0]+x[1])) > len(str(x[1])))
 print(winners)
