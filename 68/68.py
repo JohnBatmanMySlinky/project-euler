10b373941270b5334650d08baad45562798b4155
John Myslinski <johnmyslinski@Johns-MacBook-Air.local>
2020-11-15 16:22:31 -0500

    solved paper and pencil too lazy to code

---
+++ 68/68.py
@@ -0,0 +1,6 @@
+# magic 5gon
+# outer ring starts with the smallest value
+# so to max we would have 6, 7, 8, 9, 10 in the outer nodes
+# starting with 6
+# that leaves 5,4,3,2,1 on the inner nodes.
+# that seems brute forceable
