0ae3056c0b5eb5296e41948b2e94056434fac4b4
John Myslinski <johnmyslinski@Johns-MacBook-Air.local>
2020-11-28 10:36:23 -0500

    71 typo fix

--- 71/71.py
+++ 71/71.py
@@ -6,10 +6,10 @@
 # trunc(d * 3 / 7)
 # then we need to find the n such that n/d is max
 
-answer = [1,3]                                                         
+answer = [1,3]
 for d in range(1,1000000):
     if d != 7:
-    n = d * 3 / 7                                                       # using ints to will trunc naturally
+        n = d * 3 / 7                                                       # using ints to will trunc naturally
         better_sol_check = 1.0 * n / d > 1.0 * answer[0] / answer[1]    # check our solution is in fact closer to 3/7
         not_mult_of_check = (n % 3 != 0) | (d % 7 != 0)                 # check we don't have a 6/14...
         if better_sol_check & not_mult_of_check:
